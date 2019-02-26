d4d2c4ab8c568c36c92016f8f91976aabe17d0f8
ITHelp.Stream <ithelp.stream@gmail.com>
2019-02-26 08:35:45 +0530

    added new groovy

--- src/main/com/service/config/jenkinsConfig.groovy
+++ src/main/com/service/config/jenkinsConfig.groovy
@@ -6,7 +6,7 @@ def setupJenkinsConfig(){
 
 try {
      wrap([$class: 'AnsiColorBuildWrapper']) {
-	 properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '10', artifactNumToKeepStr: '10', daysToKeepStr: '10', numToKeepStr: '10')), gitLabConnection(''), [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], [$class: 'ThrottleJobProperty', categories: [], limitOneJobWithMatchingParams: false, maxConcurrentPerNode: 0, maxConcurrentTotal: 0, paramsToUseForLimit: '', throttleEnabled: false, throttleOption: 'project']])
+	 properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '2', artifactNumToKeepStr: '2', daysToKeepStr: '2', numToKeepStr: '2')), gitLabConnection(''), [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], [$class: 'ThrottleJobProperty', categories: [], limitOneJobWithMatchingParams: false, maxConcurrentPerNode: 0, maxConcurrentTotal: 0, paramsToUseForLimit: '', throttleEnabled: false, throttleOption: 'project']])
 	 }
 	 }
 	 catch (error) {
