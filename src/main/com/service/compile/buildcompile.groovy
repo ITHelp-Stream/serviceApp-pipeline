a1f1bae4cc05a538fec18b99625f794cf7a5533c
ITHelp-Stream <41923069+ITHelp-Stream@users.noreply.github.com>
2019-03-08 12:30:35 +0530

    deleted color codes

--- src/main/com/service/compile/buildcompile.groovy
+++ src/main/com/service/compile/buildcompile.groovy
@@ -6,12 +6,12 @@ def compileBuild(POM_PATH, MAVEN_VERSION, MVN_GOALS){
     try {
     wrap([$class: 'AnsiColorBuildWrapper']) {
 	sh "'${MAVEN_VERSION}'/bin/mvn -f '${POM_PATH}' ${MVN_GOALS}"
-    print "\u001B[32m[INFO]: Successfully Executing the Build..."
+    print "[INFO]: Successfully Executing the Build..."
 	}
 	}
     catch (error) {
      wrap([$class: 'AnsiColorBuildWrapper']) {
-        print "\u001B[41m[ERROR]: Build is failed.. please check the console logs"
+        print "[ERROR]: Build is failed.. please check the console logs"
         throw error
      }
    }
