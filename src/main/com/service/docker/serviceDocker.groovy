5f3f2a041d9e703a51e25294eee9ba58348d8856
ITHelp-Stream <41923069+ITHelp-Stream@users.noreply.github.com>
2019-03-08 12:23:25 +0530

    Update serviceDocker.groovy

--- src/main/com/service/docker/serviceDocker.groovy
+++ src/main/com/service/docker/serviceDocker.groovy
@@ -5,14 +5,14 @@ package main.com.service.docker
 def createDockerImage(String APP_IMAGE_NAME , String DOCKERFILE_PATH){
 try {
     wrap([$class: 'AnsiColorBuildWrapper']) {
-	print "\u001B[51mINFO => triggering docker build for $APP_IMAGE_NAME please wait..."
+	print "INFO => triggering docker build for $APP_IMAGE_NAME please wait..."
         sh "cd ${WORKSPACE}"
 	sh "docker build -t ${APP_IMAGE_NAME} --file=${DOCKERFILE_PATH} ${WORKSPACE}"
 	}
 	}
     catch (error) {
      wrap([$class: 'AnsiColorBuildWrapper']) {
-        print "\u001B[41m[ERROR]: Failed to create Image.. please check the console logs"
+        print "[ERROR]: Failed to create Image.. please check the console logs"
         throw error
      }
    }
